3847dc8a5292b1dadf7321dc3622f25f9c5f0474
Dzakhveisha <Dzakhveisha@gmail.com>
2021-12-20 00:32:37 +0300

    delete third party file

--- controller/src/main/java/com/epam/esm/controller/app/Main.java
+++
@@ -1,7 +0,0 @@
-package com.epam.esm.controller.app;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("insert into users (user_id, certificate_id, u_password, role) values (347, 52, 'CgG9lrTWhZ', 2);");
-    }
-}
